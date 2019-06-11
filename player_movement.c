28568ee6ae38c0749c59800ce7dbe3e7a5c54144
MH2033 <mh.akbare@gmail.com>
2019-06-11 16:52:43 +0430

    Tiny refactor

--- player_movement.c
+++ player_movement.c
@@ -10,139 +10,139 @@ extern struct put dblock;
 extern int TX[2],TY[2];
 extern char **game_map;
 
-void move_up(int x_p_x ,int x_p_y,char obj,int score[],int i){
-    if (game_map[x_p_x][x_p_y] == obj) {
-        if (game_map[x_p_x - 1][x_p_y] == ' ') {
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x - 1][x_p_y] = obj;
-        } else if (game_map[x_p_x - 1][x_p_y] == point.symbol) {
+void move_up(int x ,int y,char obj,int score[],int i){
+    if (game_map[x][y] == obj) {
+        if (game_map[x - 1][y] == ' ') {
+            game_map[x][y] = ' ';
+            game_map[x - 1][y] = obj;
+        } else if (game_map[x - 1][y] == point.symbol) {
             score[i] += point.score;
-            game_map[x_p_x - 1][x_p_y] = ' ';
+            game_map[x - 1][y] = ' ';
             random_gen(1,point.symbol);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x - 1][x_p_y] = obj;
+            game_map[x][y] = ' ';
+            game_map[x - 1][y] = obj;
         }
-        else if (game_map[x_p_x - 1][x_p_y] == 5) {
+        else if (game_map[x - 1][y] == 5) {
             score[i] += 5;
-            game_map[x_p_x - 1][x_p_y] = ' ';
+            game_map[x - 1][y] = ' ';
             rand1(1,5);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x - 1][x_p_y] = obj;
-        }else if(game_map[x_p_x - 1][x_p_y] == 6){
-            game_map [x_p_x - 1][x_p_y] = ' ';
-            game_map [x_p_x][x_p_y] = ' ';
+            game_map[x][y] = ' ';
+            game_map[x - 1][y] = obj;
+        }else if(game_map[x - 1][y] == 6){
+            game_map [x - 1][y] = ' ';
+            game_map [x][y] = ' ';
             for (int j = 0; j < 2; j++) {
                 if(game_map[TX[j]][TY[j]] == 6){
                     game_map[TX[j]][TY[j]] = ' ';
-                    x_p_x = TX[j];
-                    x_p_y = TY[j];
+                    x = TX[j];
+                    y = TY[j];
                     rand1(2,6);
                 }
             }
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y] = obj;
+            game_map[x][y] = ' ';
+            game_map[x][y] = obj;
         }
     }
 }
-void move_down(int x_p_x ,int x_p_y,char obj,int score[],int i){
-    if (game_map[x_p_x][x_p_y] == obj) {
-        if (game_map[x_p_x +1][x_p_y] == ' ') {
-            game_map[x_p_x][x_p_y] = 0;
-            game_map[x_p_x + 1][x_p_y] = obj;
-        } else if (game_map[x_p_x + 1][x_p_y] == point.symbol) {
+void move_down(int x ,int y,char obj,int score[],int i){
+    if (game_map[x][y] == obj) {
+        if (game_map[x +1][y] == ' ') {
+            game_map[x][y] = 0;
+            game_map[x + 1][y] = obj;
+        } else if (game_map[x + 1][y] == point.symbol) {
             score[i] += point.score;
-            game_map[x_p_x + 1][x_p_y] = ' ';
+            game_map[x + 1][y] = ' ';
             random_gen(1, point.symbol);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x + 1][x_p_y] = obj;
+            game_map[x][y] = ' ';
+            game_map[x + 1][y] = obj;
         }
-        else if (game_map[x_p_x + 1][x_p_y] == 5) {
+        else if (game_map[x + 1][y] == 5) {
             score[i] += 5;
-            game_map[x_p_x + 1][x_p_y] = ' ';
+            game_map[x + 1][y] = ' ';
             rand1(1,5);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x + 1][x_p_y] = obj;
-        }else if(game_map[x_p_x + 1][x_p_y] == 6){
-            game_map [x_p_x + 1][x_p_y] = ' ';
-            game_map [x_p_x][x_p_y] = ' ';
+            game_map[x][y] = ' ';
+            game_map[x + 1][y] = obj;
+        }else if(game_map[x + 1][y] == 6){
+            game_map [x + 1][y] = ' ';
+            game_map [x][y] = ' ';
             for (int j = 0; j < 2; j++) {
                 if(game_map[TX[j]][TY[j]] == 6){
                     game_map[TX[j]][TY[j]] = ' ';
-                    x_p_x = TX[j];
-                    x_p_y = TY[j];
+                    x = TX[j];
+                    y = TY[j];
                 }
             }
             rand1(2,6);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y] = obj;
+            game_map[x][y] = ' ';
+            game_map[x][y] = obj;
         }
     }
 }
-void move_right(int x_p_x ,int x_p_y,char obj, int score[],int i) {
-    if (game_map[x_p_x][x_p_y] == obj) {
-        if (game_map[x_p_x][x_p_y +1] == ' ') {
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y +1] = obj;
-        } else if (game_map[x_p_x][x_p_y +1] == point.symbol) {
+void move_right(int x ,int y,char obj, int score[],int i) {
+    if (game_map[x][y] == obj) {
+        if (game_map[x][y +1] == ' ') {
+            game_map[x][y] = ' ';
+            game_map[x][y +1] = obj;
+        } else if (game_map[x][y +1] == point.symbol) {
             score[i] += point.score;
-            game_map[x_p_x][x_p_y + 1] = ' ';
+            game_map[x][y + 1] = ' ';
             random_gen(1, point.symbol);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y +1] = obj;
+            game_map[x][y] = ' ';
+            game_map[x][y +1] = obj;
         }
-        else if (game_map[x_p_x][x_p_y +1]== 5) {
+        else if (game_map[x][y +1]== 5) {
             score[i] += 5;
-            game_map[x_p_x][x_p_y +1] = ' ';
+            game_map[x][y +1] = ' ';
             rand1(1,5);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y +1] = obj;
-        }else if(game_map[x_p_x][x_p_y + 1] == 6){
-            game_map [x_p_x][x_p_y + 1] = ' ';
-            game_map [x_p_x][x_p_y] = ' ';
+            game_map[x][y] = ' ';
+            game_map[x][y +1] = obj;
+        }else if(game_map[x][y + 1] == 6){
+            game_map [x][y + 1] = ' ';
+            game_map [x][y] = ' ';
             for (int j = 0; j < 2; j++) {
                 if(game_map[TX[j]][TY[j]] == 6){
                     game_map[TX[j]][TY[j]] = ' ';
-                    x_p_x = TX[j];
-                    x_p_y = TY[j];
+                    x = TX[j];
+                    y = TY[j];
                     rand1(2,6);
                 }
             }
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y] = obj;
+            game_map[x][y] = ' ';
+            game_map[x][y] = obj;
         }
     }
 }
-void move_left(int x_p_x ,int x_p_y,char obj,int score[],int i) {
-    if (game_map[x_p_x][x_p_y] == obj) {
-        if (game_map[x_p_x][x_p_y -1] == ' ') {
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y - 1] = obj;
-        } else if (game_map[x_p_x][x_p_y -1] == point.symbol) {
+void move_left(int x ,int y,char obj,int score[],int i) {
+    if (game_map[x][y] == obj) {
+        if (game_map[x][y -1] == ' ') {
+            game_map[x][y] = ' ';
+            game_map[x][y - 1] = obj;
+        } else if (game_map[x][y -1] == point.symbol) {
             score[i] += point.score;
-            game_map[x_p_x][x_p_y - 1] = ' ';
+            game_map[x][y - 1] = ' ';
             random_gen(1, point.symbol);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y -1] = obj;
+            game_map[x][y] = ' ';
+            game_map[x][y -1] = obj;
         }
-        else if (game_map[x_p_x][x_p_y -1]== 5) {
+        else if (game_map[x][y -1]== 5) {
             score[i] += 5;
-            game_map[x_p_x][x_p_y -1] = ' ';
+            game_map[x][y -1] = ' ';
             rand1(1,5);
-            game_map[x_p_x][x_p_y] = ' ';
-            game_map[x_p_x][x_p_y -1] = obj;
-        }else if(game_map[x_p_x][x_p_y -1] == 6){
-            game_map [x_p_x][x_p_y -1 ] = ' ';
-            game_map [x_p_x][x_p_y] = ' ';
+            game_map[x][y] = ' ';
+            game_map[x][y -1] = obj;
+        }else if(game_map[x][y -1] == 6){
+            game_map [x][y -1 ] = ' ';
+            game_map [x][y] = ' ';
             for (int j = 0; j < 2; j++) {
                 if(game_map[TX[j]][TY[j]] == 6){
                     game_map[TX[j]][TY[j]] = ' ';
-                    x_p_x = TX[j];
-                    x_p_y = TY[j];
+                    x = TX[j];
+                    y = TY[j];
                     rand1(2,6);
                 }
             }
-            game_map[x_p_x][x_p_y] = 0;
-            game_map[x_p_x][x_p_y] = obj;
+            game_map[x][y] = 0;
+            game_map[x][y] = obj;
         }
     }
 }
