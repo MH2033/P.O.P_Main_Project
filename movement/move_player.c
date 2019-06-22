93578a794413ee68220502ab7cafafa961219d6d
MH2033 <mh.akbare@gmail.com>
2019-06-22 16:55:45 +0430

    Add A.I movement

--- movement/move_player.c
+++ movement/move_player.c
@@ -5,11 +5,11 @@ extern struct dimension player_pos;
 extern int score[2];
 void move_player(char c){
     if(c == up)
-        move_up(player_pos.x, player_pos.y, character, score, 0);
+        move_up(&player_pos, character, score, 0);
     else if(c == down)
-        move_down(player_pos.x, player_pos.y, character, score, 0);
+        move_down(&player_pos, character, score, 0);
     else if(c == right)
-        move_right(player_pos.x, player_pos.y, character, score, 0);
+        move_right(&player_pos, character, score, 0);
     else if(c == left)
-        move_left(player_pos.x, player_pos.y, character, score, 0);
+        move_left(&player_pos, character, score, 0);
 }
--- movement/player_movement.c
+++ movement/player_movement.c
@@ -8,84 +8,165 @@ extern struct rpoint point;
 extern struct opponent opp;
 extern struct put dblock;
 extern char **game_map;
-extern struct dimension player_pos;
 
-void move_up(int x ,int y,char obj,int score[],int i) {
+void move_up(struct dimension *pos, char obj, int score[], int i) {
     if (obj == character) {
-        if (game_map[y][x] == obj) {
-            if (game_map[y - 1][x] == ' ') {
-                game_map[y][x] = ' ';
-                game_map[y - 1][x] = obj;
-                player_pos.y = y - 1;
-            } else if (game_map[y - 1][x] == point.symbol) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (game_map[pos->y - 1][pos->x] == ' ') {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y - 1][pos->x] = obj;
+                pos->y = pos->y - 1;
+            } else if (game_map[pos->y - 1][pos->x] == point.symbol) {
                 score[i] += point.score;
-                game_map[y - 1][x] = ' ';
+                game_map[pos->y - 1][pos->x] = ' ';
                 random_gen(1, point.symbol);
-                game_map[y][x] = ' ';
-                game_map[y - 1][x] = obj;
-                player_pos.y = y - 1;
-            } else if (game_map[y - 1][x] == deathblock || game_map) {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y - 1][pos->x] = obj;
+                pos->y = pos->y - 1;
+            } else if (game_map[pos->y - 1][pos->x] == deathblock || game_map) {
                 //game_over();
+            }
+        }
+    } else if (obj == opp.rival) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (opp.dest == point.symbol) {
+                if (game_map[pos->y - 1][pos->x] == ' ') {
+                    game_map[pos->y][pos->x] = ' ';
+                    game_map[pos->y - 1][pos->x] = obj;
+                    pos->y = pos->y - 1;
+                } else if (game_map[pos->y - 1][pos->x] == point.symbol) {
+                    score[i] += point.score;
+                    game_map[pos->y - 1][pos->x] = ' ';
+                    random_gen(1, point.symbol);
+                    game_map[pos->y][pos->x] = ' ';
+                    game_map[pos->y - 1][pos->x] = obj;
+                    pos->y = pos->y - 1;
+                } else if (game_map[pos->y - 1][pos->x] == deathblock || game_map) {
+                    //game_over();
+                }
             }
         }
     }
 }
-void move_down(int x ,int y,char obj,int score[],int i){
-    if(obj == character) {
-        if (game_map[y][x] == obj) {
-            if (game_map[y + 1][x] == ' ') {
-                game_map[y][x] = ' ';
-                game_map[y + 1][x] = obj;
-                player_pos.y = y + 1;
-            } else if (game_map[y + 1][x] == point.symbol) {
+void move_down(struct dimension *pos,char obj,int score[],int i) {
+    if (obj == character) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (game_map[pos->y + 1][pos->x] == ' ') {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y + 1][pos->x] = obj;
+                pos->y = pos->y + 1;
+            } else if (game_map[pos->y + 1][pos->x] == point.symbol) {
                 score[i] += point.score;
-                game_map[y + 1][x] = ' ';
+                game_map[pos->y + 1][pos->x] = ' ';
                 random_gen(1, point.symbol);
-                game_map[y][x] = ' ';
-                game_map[y + 1][x] = obj;
-                player_pos.y = y + 1;
-            } else if (game_map[y + 1][x] == deathblock) {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y + 1][pos->x] = obj;
+                pos->y = pos->y + 1;
+            } else if (game_map[pos->y + 1][pos->x] == deathblock) {
                 //game_over();
+            }
+        }
+    } else if (obj == opp.rival) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (opp.dest == point.symbol) {
+                if (game_map[pos->y][pos->x] == obj) {
+                    if (game_map[pos->y + 1][pos->x] == ' ') {
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y + 1][pos->x] = obj;
+                        pos->y = pos->y + 1;
+                    } else if (game_map[pos->y + 1][pos->x] == point.symbol) {
+                        score[i] += point.score;
+                        game_map[pos->y + 1][pos->x] = ' ';
+                        random_gen(1, point.symbol);
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y + 1][pos->x] = obj;
+                        pos->y = pos->y + 1;
+                    } else if (game_map[pos->y + 1][pos->x] == deathblock) {
+                        //game_over();
+                    }
+                }
             }
         }
     }
 }
-void move_right(int x ,int y,char obj, int score[],int i) {
-    if(obj == character) {
-        if (game_map[y][x] == obj) {
-            if (game_map[y][x + 1] == ' ') {
-                game_map[y][x] = ' ';
-                game_map[y][x + 1] = obj;
-                player_pos.x = x + 1;
-            } else if (game_map[y][x + 1] == point.symbol) {
+void move_right(struct dimension *pos,char obj, int score[],int i) {
+    if (obj == character) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (game_map[pos->y][pos->x + 1] == ' ') {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y][pos->x + 1] = obj;
+                pos->x = pos->x + 1;
+            } else if (game_map[pos->y][pos->x + 1] == point.symbol) {
                 score[i] += point.score;
-                game_map[y][x + 1] = ' ';
+                game_map[pos->y][pos->x + 1] = ' ';
                 random_gen(1, point.symbol);
-                game_map[y][x] = ' ';
-                game_map[y][x + 1] = obj;
-                player_pos.x = x + 1;
-            } else if (game_map[y][x + 1] == deathblock) {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y][pos->x + 1] = obj;
+                pos->x = pos->x + 1;
+            } else if (game_map[pos->y][pos->x + 1] == deathblock) {
                 //game_over();
+            }
+        }
+    } else if (obj == opp.rival) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (opp.dest == point.symbol) {
+                if (game_map[pos->y][pos->x] == obj) {
+                    if (game_map[pos->y][pos->x + 1] == ' ') {
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y][pos->x + 1] = obj;
+                        pos->x = pos->x + 1;
+                    } else if (game_map[pos->y][pos->x + 1] == point.symbol) {
+                        score[i] += point.score;
+                        game_map[pos->y][pos->x + 1] = ' ';
+                        random_gen(1, point.symbol);
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y][pos->x + 1] = obj;
+                        pos->x = pos->x + 1;
+                    } else if (game_map[pos->y][pos->x + 1] == deathblock) {
+                        //game_over();
+                    }
+                }
             }
         }
     }
 }
-void move_left(int x ,int y,char obj,int score[],int i) {
-    if(obj == character) {
-        if (game_map[y][x] == obj) {
-            if (game_map[y][x - 1] == ' ') {
-                game_map[y][x] = ' ';
-                game_map[y][x - 1] = obj;
-                player_pos.x = x - 1;
-            } else if (game_map[y][x - 1] == point.symbol) {
+void move_left(struct dimension *pos, char obj,int score[],int i) {
+    if (obj == character) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (game_map[pos->y][pos->x - 1] == ' ') {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y][pos->x - 1] = obj;
+                pos->x = pos->x - 1;
+            } else if (game_map[pos->y][pos->x - 1] == point.symbol) {
                 score[i] += point.score;
-                game_map[y][x - 1] = ' ';
+                game_map[pos->y][pos->x - 1] = ' ';
                 random_gen(1, point.symbol);
-                game_map[y][x] = ' ';
-                game_map[y][x - 1] = obj;
-                player_pos.x = x - 1;
-            } else if (game_map[y][x - 1] == deathblock) {
+                game_map[pos->y][pos->x] = ' ';
+                game_map[pos->y][pos->x - 1] = obj;
+                pos->x = pos->x - 1;
+            } else if (game_map[pos->y][pos->x - 1] == deathblock) {
                 //game_over();
+            }
+        }
+    } else if (obj == opp.rival) {
+        if (game_map[pos->y][pos->x] == obj) {
+            if (opp.dest == point.symbol) {
+                if (game_map[pos->y][pos->x] == obj) {
+                    if (game_map[pos->y][pos->x - 1] == ' ') {
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y][pos->x - 1] = obj;
+                        pos->x = pos->x - 1;
+                    } else if (game_map[pos->y][pos->x - 1] == point.symbol) {
+                        score[i] += point.score;
+                        game_map[pos->y][pos->x - 1] = ' ';
+                        random_gen(1, point.symbol);
+                        game_map[pos->y][pos->x] = ' ';
+                        game_map[pos->y][pos->x - 1] = obj;
+                        pos->x = pos->x - 1;
+                    } else if (game_map[pos->y][pos->x - 1] == deathblock) {
+                        //game_over();
+                    }
+                }
             }
         }
     }
