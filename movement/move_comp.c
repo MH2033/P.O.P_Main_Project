93f95939efe6098178ae460cdb7f4d14325ae085
MH2033 <mh.akbare@gmail.com>
2019-06-23 13:34:53 +0430

    Removed useless variable in move computer function

--- movement/move_comp.c
+++ movement/move_comp.c
@@ -5,7 +5,6 @@ extern int score[2];
 int flagx_k ,flagy_j ,flagy_l ,flagx_i;
 char flag_last;
 void *move_computer(void *op){
-    struct dimension *temp = (struct dimension *)op;
     if (flagx_i == 1)
         flag_last = down;
     else if (flagx_k == 1)
@@ -14,28 +13,28 @@ void *move_computer(void *op){
         flag_last = left;
     else if (flagy_j == 1)
         flag_last == right;
-    if (FindCloseFood(*temp) == up) {
+    if (FindCloseFood(*(struct dimension *)op) == up) {
         move_up(op, opp.rival, score, 1);
         flagx_k = 1;
         flagy_j = 0;
         flagy_l = 0;
         flagx_i = 0;
     }
-    else if (FindCloseFood(*temp) == down) {
+    else if (FindCloseFood(*(struct dimension *)op) == down) {
         move_down(op, opp.rival, score, 1);
         flagx_k = 0;
         flagy_j = 0;
         flagy_l = 0;
         flagx_i = 1;
     }
-    else if (FindCloseFood(*temp) == left) {
+    else if (FindCloseFood(*(struct dimension *)op) == left) {
         move_left(op, opp.rival, score, 1);
         flagx_k = 0;
         flagy_j = 0;
         flagy_l = 1;
         flagx_i = 0;
     }
-    else if (FindCloseFood(*temp) == right) {
+    else if (FindCloseFood(*(struct dimension *)op) == right) {
         move_right(op, opp.rival, score, 1);
         flagx_k = 0;
         flagy_j = 1;
