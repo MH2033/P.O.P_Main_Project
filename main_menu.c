9ce4259a79f5696351b793133347ca42507ee2c3
MH2033 <mh.akbare@gmail.com>
2019-07-02 20:08:54 +0430

    Removed scoreboard

--- main_menu.c
+++ main_menu.c
@@ -7,7 +7,7 @@ extern struct dimension default_term_size;
 WINDOW *game_menu;
 HSTREAM main_background;
 int show_main_menu(){
-    static char mesg[5][30] = {"Arcade Game Console", "Play", "Scoreboard", "Background Music Off", "Exit"};
+    static char mesg[4][30] = {"Arcade Game Console", "Play!", "Background Music Off", "Exit"};
     init_pair(1, COLOR_YELLOW, COLOR_BLACK);
     curs_set(0);
     noecho();
@@ -23,7 +23,7 @@ int show_main_menu(){
     mvwprintw(main_menu, 0,(2*default_term_size.x/3-strlen(mesg[0]))/2,"%s",mesg[0]);
     wattroff(main_menu, COLOR_PAIR(1));
     int ch = 0, i = 0;
-    for(i=1; i<5; i++) {
+    for(i=1; i<4; i++) {
         if(i == 1)
             wattron(main_menu, A_STANDOUT);
         else
@@ -32,7 +32,7 @@ int show_main_menu(){
     }
     wrefresh(main_menu);
     i = 1;
-    if(BASS_ChannelIsActive(main_background) != BASS_ACTIVE_PLAYING && strcmp(mesg[3], "Background Music Off") == 0)
+    if(BASS_ChannelIsActive(main_background) != BASS_ACTIVE_PLAYING && strcmp(mesg[2], "Background Music Off") == 0)
         BASS_ChannelPlay(main_background, FALSE);
     while(1) {
         ch = getch();
@@ -40,11 +40,11 @@ int show_main_menu(){
         switch (ch) {
             case KEY_UP:
                 i--;
-                i = (i < 1) ? 4 : i;
+                i = (i < 1) ? 3 : i;
                 break;
             case KEY_DOWN:
                 i++;
-                i = (i > 4) ? 1 : i;
+                i = (i > 3) ? 1 : i;
                 break;
         }
         wattron(main_menu, A_STANDOUT);
@@ -53,26 +53,26 @@ int show_main_menu(){
         wrefresh(main_menu);
         if(ch == '\n' && i == 1)
             break;
-        else if(ch == '\n' && i == 3){
-            if(strcmp(mesg[3], "Background Music Off") == 0){
-                strcpy(mesg[3], "Background Music On ");
+        else if(ch == '\n' && i == 2){
+            if(strcmp(mesg[2], "Background Music Off") == 0){
+                strcpy(mesg[2], "Background Music On ");
                 BASS_ChannelStop(main_background);
             } else{
-                strcpy(mesg[3], "Background Music Off");
+                strcpy(mesg[2], "Background Music Off");
                 BASS_ChannelPlay(main_background, FALSE);
             }
-            wmove(main_menu, 2*(3+1), 0);
+            wmove(main_menu, 2*(2+1), 0);
             wclrtoeol(main_menu);
             box(main_menu, 0, 0);
             wattron(main_menu, COLOR_PAIR(1));
             mvwprintw(main_menu, 0,(2 * default_term_size.x / 3 - strlen(mesg[0])) / 2,"%s",mesg[0]);
             wattroff(main_menu, COLOR_PAIR(1));
             wattron(main_menu, A_STANDOUT);
-            mvwprintw(main_menu, 2*(3+1), (2 * default_term_size.x / 3 - strlen(mesg[3])) / 2, "%s", mesg[3]);
+            mvwprintw(main_menu, 2*(2+1), (2 * default_term_size.x / 3 - strlen(mesg[2])) / 2, "%s", mesg[2]);
             wattroff(main_menu, A_STANDOUT);
             wrefresh(main_menu);
         }
-        else if(ch == '\n' && i == 4)
+        else if(ch == '\n' && i == 3)
             _Exit(0);
     }
     delwin(main_menu);
