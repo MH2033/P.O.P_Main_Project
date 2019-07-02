7e29a6544ef7f5ec202e39b472d21d5db625ae6a
pooya <pooyafekri79@gmail.com>
2019-07-02 13:48:19 +0430

    A_I

--- A_I.c
+++ A_I.c
@@ -4,94 +4,94 @@ extern char **game_map;
 extern char solidblock, deathblock, moveblock, wall, object;
 extern char up, down, left, right, character, Exit;
 extern struct opponent opp;
-int A_I(int i,int j, struct need * ops) {
+int A_I(int i,int j, struct need * opps){
     int mx, my,c2;
-    int x_p_y = ops->ops.y;
-    int  x_p_x = (ops)->ops.x;
-    debug_output(ops->opp_s.flagx_i,ops->opp_s.flagx_k,ops->opp_s.flagy_l,ops->opp_s.flagy_j,opp.dest);
-    if (((mx = i - x_p_y) < 0)&&(ops->opp_s.flagx_k ==0) && (game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)) {
+    int x_p_y = opps->ops.y;
+    int  x_p_x = opps->ops.x;
+    debug_output(opps->opp_s.flagx_i,opps->opp_s.flagx_k,opps->opp_s.flagy_l,opps->opp_s.flagy_j,opp.dest);
+    if (((mx = i - x_p_y) < 0)&&(opps->opp_s.flagx_k ==0) && (game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)) {
         c2 = up;
-        ops->opp_s.flagx_k = 1;
-        ops->opp_s.flagy_j =0;
-        ops->opp_s.flagy_l =0;
-    } else if (((my = j - x_p_x) > 0&& ops->opp_s.flagy_l ==0) && (game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)) {
+        opps->opp_s.flagx_k = 1;
+        opps->opp_s.flagy_j =0;
+        opps->opp_s.flagy_l =0;
+    } else if (((my = j - x_p_x) > 0&& opps->opp_s.flagy_l ==0) && (game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)) {
         c2 = right;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
-    } else if ((((mx = i - x_p_y) > 0)&& (ops->opp_s.flagx_k == 0)) && (game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)) {
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
+    } else if ((((mx = i - x_p_y) > 0)&& (opps->opp_s.flagx_k == 0)) && (game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)) {
         c2 = down;
-        ops->opp_s.flagy_l = 0;
-        ops->opp_s.flagx_i =1;
-        ops->opp_s.flagy_j = 0;
-    } else if (((my = j - x_p_x) < 0&& ops->opp_s.flagy_j == 0) && (game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
+        opps->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_i =1;
+        opps->opp_s.flagy_j = 0;
+    } else if (((my = j - x_p_x) < 0&& opps->opp_s.flagy_j == 0) && (game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
         c2 =left;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_l = 1;
-    } else if((mx == 0&& ops->opp_s.flagx_k ==0 )&& (game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)){
-        ops->opp_s.flagy_l = 0;
-        ops->opp_s.flagx_i =1;
-        ops->opp_s.flagy_j = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_l = 1;
+    } else if((mx == 0&& opps->opp_s.flagx_k ==0 )&& (game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)){
+        opps->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_i =1;
+        opps->opp_s.flagy_j = 0;
         c2 =down;
-    } else if((my == 0&& ops->opp_s.flagy_j ==0) && (game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_l = 1;
+    } else if((my == 0&& opps->opp_s.flagy_j ==0) && (game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_l = 1;
         c2 =left;
-    } else if((mx == 0&& ops->opp_s.flagx_k ==0) && (game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)){
-        ops->opp_s.flagy_l = 0;
-        ops->opp_s.flagx_i = 1;
-        ops->opp_s.flagy_j = 0;
+    } else if((mx == 0&& opps->opp_s.flagx_k ==0) && (game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)){
+        opps->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_i = 1;
+        opps->opp_s.flagy_j = 0;
         c2 =down;
-    } else if((my == 0&& ops->opp_s.flagy_l ==0) && (game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)){
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
+    } else if((my == 0&& opps->opp_s.flagy_l ==0) && (game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)){
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
         c2 =right;
-    }else if(ops->opp_s.flagy_j==0 &&(game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
+    }else if(opps->opp_s.flagy_j==0 &&(game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest)){
         c2 = left;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_l = 1;
-    }else if(ops->opp_s.flagx_k == 0 &&(game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)){
-        ops->opp_s.flagy_l = 0;
-        ops->opp_s.flagx_i =1;
-        ops->opp_s.flagy_j = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_l = 1;
+    }else if(opps->opp_s.flagx_k == 0 &&(game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest)){
+        opps->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_i =1;
+        opps->opp_s.flagy_j = 0;
         c2 = down;
-    }else if(ops->opp_s.flagy_l== 0 &&(game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)){
+    }else if(opps->opp_s.flagy_l== 0 &&(game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest)){
         c2 = right;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
-    }else if(ops->opp_s.flagx_k == 0 &&(game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)){
-        ops->opp_s.flagx_k= 1;
-        ops->opp_s.flagy_l = 0;
-        ops->opp_s.flagy_j = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
+    }else if(opps->opp_s.flagx_k == 0 &&(game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest)){
+        opps->opp_s.flagx_k= 1;
+        opps->opp_s.flagy_l = 0;
+        opps->opp_s.flagy_j = 0;
         c2 = up;
     }else if(game_map[x_p_y][x_p_x-1] == ' ' || game_map[x_p_y][x_p_x-1] == opp.dest){
         c2 = left;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 0;
-        ops->opp_s.flagy_l = 1;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 0;
+        opps->opp_s.flagy_l = 1;
     }else if(game_map[x_p_y+1][x_p_x] == ' ' || game_map[x_p_y+1][x_p_x] == opp.dest){
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
-        ops->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
+        opps->opp_s.flagy_l = 0;
         c2 = down;
     }else if(game_map[x_p_y][x_p_x+1] == ' ' || game_map[x_p_y][x_p_x+1] == opp.dest){
         c2 = right;
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
-        ops->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
+        opps->opp_s.flagy_l = 0;
     }else if(game_map[x_p_y-1][x_p_x] == ' ' || game_map[x_p_y-1][x_p_x] == opp.dest){
-        ops->opp_s.flagx_k= 0;
-        ops->opp_s.flagx_i =0;
-        ops->opp_s.flagy_j = 1;
-        ops->opp_s.flagy_l = 0;
+        opps->opp_s.flagx_k= 0;
+        opps->opp_s.flagx_i =0;
+        opps->opp_s.flagy_j = 1;
+        opps->opp_s.flagy_l = 0;
         c2 = up;
     }
     return c2;
